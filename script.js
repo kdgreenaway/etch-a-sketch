8886dd1aaea60659ed7934d3f8fbde46c5cb49ac
kdgreenaway <66218988+kdgreenaway@users.noreply.github.com>
2025-03-03 00:52:36 -0400

    Refactor code and add new functions
    added:
    - updateEventListener()
    - handleMouseEnter()

--- script.js
+++ script.js
@@ -1,24 +1,9 @@
 
 const container = document.querySelector("#container")
-const row = document.createElement("div")
-// const square = Number(prompt("How wide?"))
 const newGrid = document.querySelector("#newGrid")
 const toggle = document.querySelector("#toggle")
+let isRandomColor = false;
 
-newGrid.addEventListener("click", () => {
-    container.textContent = " "
-    createNewGrid()
-})
-
-toggle.addEventListener("click", () => {
-    if (toggle.style.backgroundColor = "black") {
-        randomColor()
-    }
-
-    else if (toggle.style.backgroundColor = randomColor()) {
-        toggle.style.backgroundColor = "black"
-    }
-})
 
 function randomColor() {
     let r = Math.floor(Math.random() * 255)
@@ -29,55 +14,76 @@ function randomColor() {
     return color
 }
 
+
 function createGrid() {
     for (let i = 0; i < 16 * 16; i++) {
-        const row = document.createElement("div")
-        row.classList.add("cell")
-        // row.textContent = "i"
-        
-        row.style.flexBasis = "6.25%"
-        container.appendChild(row)
+        const cell = document.createElement("div")
+        cell.classList.add("cell")
+        cell.style.flexBasis = "6.25%"
+        container.appendChild(cell)
     }
-    const grid = document.querySelectorAll(".cell")
-    grid.forEach(cell => {
-        cell.addEventListener("mouseenter", () => {
-            cell.style.backgroundColor = "black"
-    })
-})
+    updateEventListener()
 }
 
-createGrid()
 
 function createNewGrid() {
     const square = Number(prompt("Grid size"))
+    if (square <= 100) {
     for (let i = 0; i < square * square; i++) {
-        const row = document.createElement("div")
-        row.classList.add("cell")
-            container.appendChild(row)
+        const cell = document.createElement("div")
+        cell.classList.add("cell")
+            container.appendChild(cell)
+    }
+    
+
+    function dynamicGrid() {
+        const cells = document.querySelectorAll(".cell")
+        
+        cells.forEach(cell => {
+            cell.style.flexBasis = `calc(100% / (${square})`;
+            })
+        }
+    }
+
+    else {
+        alert("Choose any number up to 100")
+        createNewGrid()
     }
     dynamicGrid()
-    function dynamicGrid() {
-        const divs = document.querySelectorAll(".cell")
-        
-        divs.forEach(div => {
-            div.style.flexBasis = `calc(100% / (${square})`;
-            div.addEventListener("mouseenter", () => {
-                div.style.backgroundColor = randomColor()
-            })
-        })
+    updateEventListener()
+}
+
+
+function handleMouseEnter(e) {
+    if (isRandomColor) {
+        e.target.style.backgroundColor = randomColor()
+    }
+
+    else {
+        e.target.style.backgroundColor = "black"
     }
 }
-// createNewGrid()
 
-// function dynamicGrid() {
-//     const divs = document.querySelectorAll(".cell")
-    
-//     divs.forEach(div => {
-//         div.style.flexBasis = `calc(100% / (${square})`
-//         div.addEventListener("mouseenter", () => {
-//             div.style.backgroundColor = randomColor()
-//         })
-//     })
-// }
+function updateEventListener() {
+const cells = document.querySelectorAll(".cell")
+cells.forEach (cell => {
+    cell.removeEventListener("mouseenter", handleMouseEnter)
+    cell.addEventListener("mouseenter", handleMouseEnter)
+})
+}
 
+newGrid.addEventListener("click", () => {
+    container.textContent = " "
+    createNewGrid()
+})
 
+const cells = document.querySelectorAll(".cell")
+toggle.textContent = "Black mode"
+toggle.addEventListener("click", () => {
+    isRandomColor = !isRandomColor
+    toggle.textContent = isRandomColor ? "Color mode" : "Black mode"
+    console.log(isRandomColor)
+    updateEventListener()
+})
+
+createGrid()
